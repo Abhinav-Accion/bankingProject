a579494bb9386c3ff0b6b62ebc531ed864e15e2c
Abhinav Srivastava <AL5148@accionlabs.in>
2023-04-18 15:47:01 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BankLoginComponent } from './bank-login/bank-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BankLoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bank-login/bank-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-login',
+  templateUrl: './bank-login.component.html',
+  styleUrls: ['./bank-login.component.scss']
+})
+export class BankLoginComponent {
+
+}
